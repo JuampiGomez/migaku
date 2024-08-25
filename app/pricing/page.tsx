61826e7f8ae4d426a2f8f48298acd971cb2738cc
JuampiGomez <juampi3217@gmail.com>
2024-08-25 11:53:37 -0300

    Pricing page demo finished

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,4 +1,6 @@
-import PricingCard from "@/components/pricing/PricingCard";
+import PricingCard, {
+  EearlyAccessCard,
+} from "@/components/pricing/PricingCard";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
@@ -9,20 +11,54 @@ const page = (props: Props) => {
   return (
     <main>
       <div className="px-6">
-        <h1 className="font-extrabold text-center mt-10 mb-5 text-5xl">
+        <h1 className="font-extrabold text-center mt-10 mb-5 text-5xl md:text-7xl lg:text-8xl">
           Choose a plan
         </h1>
 
-        <p className="text-center font-medium">
+        <p className="text-center font-medium max-w-[30ch] mx-auto md:max-w-[40ch] md:text-lg lg:max-w-full">
           Start your 10 day free trial or sign up immediately for unlimited
           access
         </p>
-        <Link href={"/signup"} className="my-5 block max-w-max mx-auto">
-          <Button>START 10 DAY FREE TRIAL</Button>
+        <Link href={"/signup"} className="mt-5 mb-10 block max-w-max mx-auto">
+          <Button className="md:text-lg">START 10 DAY FREE TRIAL</Button>
         </Link>
       </div>
-      <section className="px-6">
-        <PricingCard />
+      <section className="px-6 grid gap-10 md:grid-cols-2 mr-3 md:max-w-[910px] md:mx-auto lg:max-w-full lg:grid-cols-3 ">
+        <PricingCard
+          title="Standard"
+          subtitle="Our most popular plan."
+          buttonText="START EXPLORING"
+          yearlyPrice="$87"
+          monthlyPrice="$9"
+          backgroundClasses="bg-[#EDE3FF] dark:bg-[#202047]"
+          featuresList={[
+            "Study all 9 available languages",
+            "Access to all of our courses",
+            "Create unlimited interactive flashcards",
+            "Powerful new AI features",
+            "Many subtitle and video playback options",
+            "Look up words & track your progress",
+            "iOS & Android apps for studying on-the-go",
+            "Anki support",
+          ]}
+        />
+
+        <EearlyAccessCard />
+
+        <PricingCard
+          title="Lifetime"
+          subtitle="The most cost-effective plan."
+          buttonText="GET ENDLESS BENEFITS"
+          yearlyPrice="..."
+          monthlyPrice="$399"
+          isLimitedTimeOffer={true}
+          backgroundClasses="bg-[#EDE3FF] dark:bg-[#202047] md:col-span-2 lg:col-span-1 lg:mr-3  "
+          featuresList={[
+            "Lifetime access",
+            "Invest in our success",
+            "Everything included in Early Access",
+          ]}
+        />
       </section>
     </main>
   );
--- components/pricing/PricingCard.tsx
+++ components/pricing/PricingCard.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import Image from "next/image";
@@ -8,64 +9,111 @@ import {
   CardBorderLeftAndTop,
 } from "../ui/cardBorder3d";
 
-type Props = {};
+type Props = {
+  monthlyPrice: string;
+  yearlyPrice: string;
+  featuresList: string[];
+  title: string;
+  subtitle: string;
+  isLimitedTimeOffer?: boolean;
+  buttonText: string;
+  backgroundClasses: string;
+};
 
-const PricingCard = (props: Props) => {
+const PricingCard = ({
+  monthlyPrice,
+  yearlyPrice,
+  featuresList,
+  title,
+  subtitle,
+  isLimitedTimeOffer = false,
+  buttonText,
+  backgroundClasses,
+}: Props) => {
+  const [isAnualPrice, setIsAnualPrice] = useState(false);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsAnualPrice(event.target.checked);
+  };
+
+  function useDarkMode() {
+    if (typeof window !== "undefined") {
+      return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    }
+    return false; // Default to light mode if window is not defined
+  }
+
   return (
-    <aside className="bg-[#EDE3FF] dark:bg-[#202047] px-[18px] py-8 relative grid gap-8">
+    //
+    <aside
+      className={`${backgroundClasses} px-[18px] py-8 relative flex flex-col gap-8 w-full max-w-[390px] mx-auto`}
+    >
       <div className="grid gap-4">
-        <h2 className="text-center font-extrabold text-migaku-dark-text dark:text-white text-2xl">
-          Standard
+        <h2 className="text-center font-extrabold text-migaku-dark-text dark:text-white text-2xl md:text-4xl">
+          {title}
         </h2>
-        <span className="block text-center">Our most popular plan.</span>
+        <span className="block text-center md:text-lg">{subtitle}</span>
         <Link href={"/signup"} className="max-w-max mx-auto">
-          <Button>START EXPLORING</Button>
+          <Button className="md:text-lg">{buttonText}</Button>
         </Link>
       </div>
 
       <div className="grid gap-4">
-        <span className="block text-center font-extrabold text-[32px] text-migaku-dark-text dark:text-white ">
-          $9
+        <span className="block text-center font-extrabold text-[32px] text-migaku-dark-text dark:text-white md:text-6xl ">
+          {isAnualPrice ? yearlyPrice : monthlyPrice}
         </span>
-        <span className="block text-center  text-migaku-dark-text dark:text-white font-medium">
-          Monthly
+        <span className="block text-center  text-migaku-dark-text dark:text-white font-medium md:text-lg">
+          {isAnualPrice ? "Yearly" : "Monthly"}
         </span>
-        <p className="text-center text-sm text-gray-700 dark:text-gray-400">
+        <p className="text-center text-sm text-gray-700 dark:text-gray-400 md:text-base">
           14-day money-back guarantee. <br />
           Cancel anytime.
         </p>
       </div>
 
-      <div className="flex justify-center gap-3">
-        <input type="checkbox" />
-        <span className="text-[12px] font-bold">Yearly price</span>
-        <span className="text-[12px] text-gray-700 dark:text-gray-400">
-          Save 20%
-        </span>
-      </div>
+      {isLimitedTimeOffer ? (
+        <div className="">
+          <span className="block max-w-max mx-auto font-bold text-migaku-dark-text text-[12px] md:text-sm bg-[#F2FFA0] px-3 py-2  rounded-2xl">
+            Limited Time Offer
+          </span>
+        </div>
+      ) : (
+        <div className="flex justify-center items-center gap-3">
+          <input
+            type="checkbox"
+            onChange={handleCheckboxChange}
+            checked={isAnualPrice}
+          />
+          <span className="text-[12px] md:text-sm  font-bold">
+            Yearly price
+          </span>
+          <span className="text-[12px] md:text-sm text-gray-700 dark:text-gray-400">
+            Save 20%
+          </span>
+        </div>
+      )}
 
       <div className="">
-        <ul className="text-[12px] flex flex-col gap-3 items-center">
-          <li className="flex gap-2">
-            <Image src={"/checkLight.svg"} width={20} height={20} alt="check" />
-            Study all 9 available languages
-          </li>
-          <li className="flex gap-2">
-            <Image src={"/checkLight.svg"} width={20} height={20} alt="check" />
-            Access to all of our courses
-          </li>
-          <li className="flex gap-2">
-            <Image src={"/checkLight.svg"} width={20} height={20} alt="check" />
-            Create unlimited interactive flashcards
-          </li>
-          <li className="flex gap-2">
-            <Image src={"/checkLight.svg"} width={20} height={20} alt="check" />
-            Access to all of our courses
-          </li>
-          <li className="flex gap-2">
-            <Image src={"/checkLight.svg"} width={20} height={20} alt="check" />
-            Study all 9 available languages
-          </li>
+        <ul className="text-[12px] md:text-sm flex flex-col gap-3 items-center text-center">
+          {featuresList.map((feature, i) => (
+            <li key={i} className="flex gap-2">
+              <Image
+                className="hidden dark:block"
+                src={"/checkLight.svg"}
+                width={20}
+                height={20}
+                alt="check"
+              />
+              <Image
+                className="block dark:hidden"
+                src={"/checkDark.svg"}
+                width={20}
+                height={20}
+                alt="check"
+              />
+              {feature}
+            </li>
+          ))}
         </ul>
       </div>
 
@@ -77,3 +125,87 @@ const PricingCard = (props: Props) => {
 };
 
 export default PricingCard;
+
+export const EearlyAccessCard = () => {
+  const [isAnualPrice, setIsAnualPrice] = useState(false);
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsAnualPrice(event.target.checked);
+  };
+
+  return (
+    <aside
+      className={`bg-[#F2FFA0] px-[18px] py-8 relative flex flex-col gap-8  w-full max-w-[390px] mx-auto`}
+    >
+      <div className="earlyaccess-bg absolute inset-0  "></div>
+      <div className="grid gap-4 z-10">
+        <h2 className="text-center font-extrabold text-migaku-dark-text  text-2xl md:text-4xl">
+          Early Acces
+        </h2>
+        <span className="block text-migaku-dark-text text-center md:text-lg">
+          Get early access to new features.
+        </span>
+        <Link href={"/signup"} className="max-w-max mx-auto">
+          <Button className="md:text-lg">LIVE AT THE EDGE</Button>
+        </Link>
+      </div>
+
+      <div className="grid gap-4 z-10">
+        <span className="block text-center font-extrabold text-[32px] text-migaku-dark-text md:text-6xl  ">
+          {isAnualPrice ? "$135" : "$14"}
+        </span>
+        <span className="block text-center  text-migaku-dark-text  font-medium md:text-lg">
+          {isAnualPrice ? "Yearly" : "Monthly"}
+        </span>
+        <p className="text-center text-sm text-gray-700 md:text-base ">
+          14-day money-back guarantee. <br />
+          Cancel anytime.
+        </p>
+      </div>
+
+      <div className="flex justify-center items-center gap-3 z-10">
+        <input
+          type="checkbox"
+          onChange={handleCheckboxChange}
+          checked={isAnualPrice}
+        />
+        <span className="text-[12px] md:text-sm text-migaku-dark-text font-bold">
+          Yearly price
+        </span>
+        <span className="text-[12px] md:text-sm  text-gray-400">Save 20%</span>
+      </div>
+
+      <div className="z-10">
+        <ul className="text-[12px]  md:text-sm text-migaku-dark-text flex flex-col gap-3 items-center text-center">
+          <li className="flex gap-2">
+            <Image src={"/checkDark.svg"} width={20} height={20} alt="check" />
+            Access hot new features early
+          </li>
+          <li className="flex gap-2">
+            <Image src={"/checkDark.svg"} width={20} height={20} alt="check" />
+            Be the first to check out new courses
+          </li>
+          <li className="flex gap-2">
+            <Image src={"/checkDark.svg"} width={20} height={20} alt="check" />
+            Get hands on with testing and feedback
+          </li>
+          <li className="flex gap-2">
+            <Image src={"/checkDark.svg"} width={20} height={20} alt="check" />
+            Access to hidden Discord channel
+          </li>
+          <li className="flex gap-2">
+            <Image src={"/checkDark.svg"} width={20} height={20} alt="check" />
+            Support what we do
+          </li>
+          <li className="flex gap-2">
+            <Image src={"/checkDark.svg"} width={20} height={20} alt="check" />
+            Everything included in Standard
+          </li>
+        </ul>
+      </div>
+
+      <CardBorder3dBottom bg="bg-[#DFF4FF] dark:bg-[#2B2B60]" />
+      <CardBorder3dRight bg="bg-[#DFF4FF] dark:bg-[#2B2B60]" />
+      <CardBorderLeftAndTop />
+    </aside>
+  );
+};
